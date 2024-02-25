2eacfcd3a38b9bfd9391c96da18a869387a7aacd
jasonhao518 <jasonhao518@gmail.com>
2024-02-25 14:52:12 +0100

    Update NavLinks.tsx

--- client/src/components/Nav/NavLinks.tsx
+++ client/src/components/Nav/NavLinks.tsx
@@ -10,17 +10,19 @@ import { useAuthContext } from '~/hooks/AuthContext';
 import useAvatar from '~/hooks/Messages/useAvatar';
 import { ExportModal } from './ExportConversation';
 import { LinkIcon, GearIcon } from '~/components';
+import { LogOutIcon } from '../svg';
 import { useLocalize } from '~/hooks';
 import Settings from './Settings';
 import NavLink from './NavLink';
 import Logout from './Logout';
 import { cn } from '~/utils/';
 import store from '~/store';
+import { OrganizationSwitcher, SignOutButton, SignedIn } from '@clerk/clerk-react';
 
 function NavLinks() {
   const localize = useLocalize();
   const location = useLocation();
-  const { user, isAuthenticated } = useAuthContext();
+  const { user, isAuthenticated, logout } = useAuthContext();
   const { data: startupConfig } = useGetStartupConfig();
   const balanceQuery = useGetUserBalance({
     enabled: !!isAuthenticated && startupConfig?.checkBalance,
@@ -130,8 +132,27 @@ function NavLinks() {
                   />
                 </Menu.Item>
                 <div className="my-1 h-px bg-white/20" role="none" />
+                <SignedIn>
+                  <Menu.Item as="div">
+                    <OrganizationSwitcher
+                      afterSelectPersonalUrl={(user) => {
+                        logout();
+                        return 'ok';
+                      }}
+                      afterSelectOrganizationUrl={(user) => {
+                        logout();
+                        return 'ok';
+                      }}
+                    />
+                  </Menu.Item>
+                </SignedIn>
                 <Menu.Item as="div">
-                  <Logout />
+                  <SignOutButton signOutCallback={() => logout()}>
+                    <button className="flex w-full cursor-pointer items-center gap-3 px-3 py-3 text-sm text-white transition-colors duration-200 hover:bg-gray-700">
+                      <LogOutIcon />
+                      {localize('com_nav_log_out')}
+                    </button>
+                  </SignOutButton>
                 </Menu.Item>
               </Menu.Items>
             </Transition>
