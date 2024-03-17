e25f5a68f714bb183af8d4e6c8d5eb22a7b79353
jasonhao518 <jasonhao518@gmail.com>
2024-03-17 18:19:05 +0100

    restore redis config

--- api/cache/keyvRedis.js
+++ api/cache/keyvRedis.js
@@ -7,12 +7,7 @@ const { REDIS_URI, USE_REDIS } = process.env;
 let keyvRedis;
 
 if (REDIS_URI && isEnabled(USE_REDIS)) {
-  keyvRedis = new KeyvRedis({
-    uri: REDIS_URI,
-    tls: {
-      rejectUnauthorized: false,
-    },
-  });
+  keyvRedis = new KeyvRedis(REDIS_URI, { useRedisSets: false });
   keyvRedis.on('error', (err) => logger.error('KeyvRedis connection error:', err));
   keyvRedis.setMaxListeners(20);
   logger.info(
